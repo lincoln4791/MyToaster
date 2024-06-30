c1490eda2dcaad8dfe24ac94e636d859b06be65d
Lincoln <mahmuduulkarim@gmail.com>
2024-06-30 14:52:03 +0600

    init

---
+++ app/src/main/java/com/example/mytoaster/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.mytoaster
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn).setOnClickListener {
+            showToast()
+        }
+
+    }
+
+    fun showToast(){
+        Toast.makeText(this,"ggwp",Toast.LENGTH_LONG).show()
+    }
+}
